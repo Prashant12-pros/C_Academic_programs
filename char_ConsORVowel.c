eb436aea7665d10388a73e123497660505c5819f
Prashant Kumar <prashant25lamborghini@gmail.com>
2021-06-05 16:26:55 +0530

    commit

--- char_ConsORVowel.c
+++ char_ConsORVowel.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-
+#include <ctype.h>
+/*
 int main(){
     char alphabet[27] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
     char a;
@@ -13,5 +14,32 @@ int main(){
     else{
         printf("The letter entered is a consonant!");
     }
+    return 0;
+}
+*/
+int main(){
+    char ch;
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+    ch = toupper(ch); // 
+    
+
+    if((ch >= 'A' && ch <= 'Z')){  // || (ch >= 'a' && ch <= 'z')){
+        switch (ch){
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U':
+                printf("Entered character is a Vowel!");
+                break;
+            default :
+                printf("Entered character is a Consonant!");
+        }
+    }
+    else{
+        printf("Enter a valid character!");
+    }
+
     return 0;
 }
