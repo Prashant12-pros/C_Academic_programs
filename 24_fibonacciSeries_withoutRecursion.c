e1f38686f63adb8cc2d244fa3d74d556105d938e
Prashant Kumar <prashant25lamborghini@gmail.com>
2021-07-19 19:30:13 +0530

    commit

--- 24_fibonacciSeries_withoutRecursion.c
+++ 24_fibonacciSeries_withoutRecursion.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 
 int main(){
-    int n1 = 0, n2 = 1, n3, terms;
+    int n1 = 0, n2 = 1, n3 = 0, terms;
     printf("Enter number of terms:");
     scanf("%d",&terms);
 
-    printf("%d  %d  ",n1, n2);
-
-    for(int i = 0; i < terms-2; i++){
+    
+    for(int i = 1; i <= terms; i++){
+        printf("%d  ",n1);
         n3 = n1+n2;
         n1 = n2;
         n2 = n3;
-        printf("%d  ",n3);
     }
     return 0;
 }
