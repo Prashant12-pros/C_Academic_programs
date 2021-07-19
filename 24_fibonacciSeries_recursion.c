d131d4086a57c8876e8510458c226b5e115d6676
Prashant Kumar <prashant25lamborghini@gmail.com>
2021-07-19 19:39:04 +0530

    impr. and efficient code for fibonacci

--- 24_fibonacciSeries_recursion.c
+++ 24_fibonacciSeries_recursion.c
@@ -1,31 +1,21 @@
 #include <stdio.h>
 
 int fibonacci(int n){
-    static int n1 = 0, n2 = 1, n3; //using static storage class as every time the function is called wihtin function, variable must retain the previous value.
+    static int n1 = 0, n2 = 1, n3 = 0; //using static storage class as every time the function is called wihtin function, variable must retain the previous value.
     if(n > 0){
+        printf(" %d  ", n1);
         n3 = n1 + n2;
         n1 = n2;
         n2 = n3;
-        printf(" %d  ", n3);
         fibonacci(n-1);             //function called recursively
     }
 }
 int main(){
-    int n1 = 0, n2 = 1, terms;
+    int terms;
     printf("Enter the number of terms:");
     scanf("%d",&terms);
 
-    printf("%d  %d  ", n1, n2); //for printing the first 2 numbers
-
-    fibonacci(terms-2);
-    // if (terms == 1)
-    //     printf("%d  ",n1);
-    // else if(terms == 2)
-    //     printf("%d  %d  ",n1,n2);
-    // else{
-    //     printf("%d  %d  ",n1, n2);
-    //     fibonacci(terms);
-    // }
+    fibonacci(terms);
     
     return 0;
 }
